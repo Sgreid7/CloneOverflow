de6ee24b17255c4c24748309ff54fda78d758d49
Austin Parvin <austin.parvin@gmail.com>
2020-04-01 13:11:48 -0400

    added title and Answers to question poco

---
+++ Migrations/20200401171042_AddedTitleAndNavigationToQuestionPoco.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CloneOverflow.Migrations
+{
+    public partial class AddedTitleAndNavigationToQuestionPoco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Questions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Questions");
+        }
+    }
+}
--- Models/Question.cs
+++ Models/Question.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 namespace CloneOverflow.Models
 {
     public class Question
     {
         public int Id { get; set; }
+        public string Title { get; set; }
         public string Content { get; set; }
         public int Score { get; set; } = 0;
         public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        // Navigation properties
+        public List<Answer> Answers { get; set; }
     }
 }
